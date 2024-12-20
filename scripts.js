1e76f9efe40c85a224f2a6ca199630aa26c5abfa
AD17277 <daniele.ambietti@hotmail.it>
2024-12-20 18:12:49 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Inizializzare la pagina e i dati dinamici (può essere usato per fetch dei dati in seguito)
+document.addEventListener('DOMContentLoaded', function() {
+    // Codice per caricare dinamicamente i dati se necessario
+});
